3d48ecc6bf3afccaec7a561d2ba25f150e070b49
Alex Gomez <eagp24@gmail.com>
2013-03-29 20:08:34 -0400

    Revert "Inicializando pruebas"
    This reverts commit c6c3c38329a3cddc8524a61527029c94c00d6cf4.

--- src/puntuacion/Score.java
+++ src/puntuacion/Score.java
@@ -8,15 +8,28 @@ public interface Score
 	 * hechos en la edicion anterior ya que los metodos deben comportarse de
 	 * la misma manera. 
 	 * */
-	public int getTotal();
-	public int getFirstScore(); 
-	public int getSecondScore();
-	public boolean isStrike();
-	public boolean isSpare(); 
-	public int getThirdScore();
-	public boolean isFirstStrike();
-	public boolean isFirstSetSpare();
-	public boolean isPerfect();
-	public void addToTotal(int num);
-	public String toString();
+	
+	public int getTotal(); // Se declara un metodo para determinar el total de un score
+
+	public int getFirstScore(); // se declara un metodo para determinar el Primer Punto
+
+	public int getSecondScore();// se declara un metodo para determinar el Segundo Punto
+
+	public boolean isStrike();// se declara un metodo para determinar si es un Strike
+
+	public boolean isSpare();// se declara un metodo para determinar si es un Spare 
+
+	public void setThirdScore(int score);// se declara un metodo para dar el valor del tercer punto.
+
+	public int getThirdScore();// se declara un metodo para determinar el tercer punto.
+
+	public boolean isFirstStrike();// se declara un metodo para determinar si el primero es strike de un score
+
+	public boolean isFirstSetSpare();// se declara un metodo para determinar si el primero es un spare en un score
+
+	public boolean isPerfect();// se declara un metodo para determinar si el juego es perfecto.
+
+	public void addToTotal(int num);// se declara un metodo para acumular los puntos de cada jugada.
+
+	public String toString();// se declara un metodo para representar como string lo que se va a imprimir.
 }
